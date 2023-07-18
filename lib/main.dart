fb210dd639091620037a8c0a6496768c60f78790
sherifhasan <sherif.hasan1994@gmail.com>
2023-07-19 01:50:58 +0200

    [Add]: search cubit bloc provider creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:journey_planner/presentation/search/search_screen.dart';
 
-void main() {
+import 'application/features/search/search_cubit.dart';
+import 'injection.dart';
+
+Future<void> main() async {
+  await setUp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => getIt.get<SearchCubit>(),
+      child: MaterialApp(
+        title: 'Journey planner',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SearchScreen(),
       ),
-      home: const Placeholder(),
     );
   }
 }
