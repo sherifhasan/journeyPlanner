5dfeb8c7f3d319704ded1806fa65c2af80914be1
sherifhasan <sherif.hasan1994@gmail.com>
2023-07-18 23:59:54 +0200

    [Add]: search cubit and search state

---
+++ lib/application/features/search/search_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:journey_planner/domain/search/models/location_model.dart';
+import 'package:journey_planner/domain/search/search_repository.dart';
+
+part 'search_cubit.freezed.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  final SearchRepository _searchRepository;
+
+  SearchCubit(
+    this._searchRepository,
+  ) : super(const SearchState.initial());
+
+  Future<void> search(String query) async {
+    emit(const SearchState.loading());
+    final results = await _searchRepository.getLocations(query);
+    if (results.isNotEmpty) {
+      final searchResults = results.map((result) => result.toEntity()).toList();
+      emit(SearchState.data(searchResults));
+    } else {
+      emit(const SearchState.empty());
+    }
+  }
+}
---
+++ lib/application/features/search/search_state.dart
@@ -0,0 +1,12 @@
+part of 'search_cubit.dart';
+
+@freezed
+class SearchState with _$SearchState {
+  const factory SearchState.initial() = _Initial;
+
+  const factory SearchState.loading() = _Loading;
+
+  const factory SearchState.data(List<LocationModel> locations) = _Data;
+
+  const factory SearchState.empty() = _Empty;
+}
