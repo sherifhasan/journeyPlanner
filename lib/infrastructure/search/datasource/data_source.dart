bae60a7fc13bf323c7145317dfd95a8b2c9c3b7f
sherifhasan <sherif.hasan1994@gmail.com>
2023-07-19 01:46:44 +0200

    [refactor]: rename remote data source to data source

--- lib/infrastructure/search/datasource/data_source.dart
+++ lib/infrastructure/search/datasource/data_source.dart
@@ -1,5 +1,5 @@
 import 'package:journey_planner/infrastructure/search/models/location_model_dto.dart';
 
-abstract class RemoteDataSource {
+abstract class DataSource {
   Future<List<LocationDto>> getLocations(String query);
 }
